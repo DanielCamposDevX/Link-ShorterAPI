ce92bed46974dcc1f7ad1420a08a867c1c125f49
DanielCamposDevX <daniel.campos.dev3@gmail.com>
2023-08-03 09:51:57 -0300

    patch/try catch

--- src/controllers/url.controller.js
+++ src/controllers/url.controller.js
@@ -25,20 +25,36 @@ export async function createShorten(req, res) {
 
 export async function getShorten(req,res){
     const { id } = req.params;
+    try{
     const response = await db.query('SELECT id,"shortURL" AS "shortUrl",url FROM urls WHERE id=$1',[id]);
     if(response.rowCount > 0){
         return res.status(200).send(response.rows[0]);
     }
     else{
         return res.sendStatus(404);
+    }}
+    catch(err){
+        res.status(500).send(err);
     }
+    
 }
 
 export async function redirect(req,res){
+    try{
     const { shortUrl } = req.params;
     const response = await db.query('SELECT url FROM urls WHERE "shortURL"=$1',[shortUrl]);
     if(response.rowCount == 0){return res.sendStatus(404)};
     const url = response.rows[0].url;
-    res.send(url);
-    ///res.redirect(url);
+    res.redirect(url);
+    }
+    catch(err){
+        res.status(500).send(err);
+    }
+}
+
+export async function DeleteShort(req,res){
+    const { authorization } = req.headers;
+    const token = authorization?.replace("Bearer ", "");
+    if (!token) { return res.status(401).send('Unauthorized') };
+    const id = db.quer
 }
