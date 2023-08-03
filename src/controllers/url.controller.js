fe909644c73f165f0f21d0b5f6286feae8cd7d58
DanielCamposDevX <daniel.campos.dev3@gmail.com>
2023-08-03 10:08:40 -0300

    feat/delete

--- src/controllers/url.controller.js
+++ src/controllers/url.controller.js
@@ -14,7 +14,7 @@ export async function createShorten(req, res) {
         if(user.rowCount == 0){return res.status(401).send('Unauthorized')};
         const userdata = user.rows[0];
         const shortUrl = nanoid(10);
-        await db.query('INSERT INTO urls ("shortURL",url,"visitCount","userID") VALUES($1,$2,$3,$4)',[shortUrl,url,null,userdata.userId]);
+        await db.query('INSERT INTO urls ("shortURL",url,"visitCount","userId") VALUES($1,$2,$3,$4)',[shortUrl,url,null,userdata.userId]);
         const response = await db.query('SELECT id FROM urls WHERE "shortURL"=$1',[shortUrl]);
         return res.status(201).send({id:response.rows[0].id,shortUrl});
     }
@@ -53,8 +53,21 @@ export async function redirect(req,res){
 }
 
 export async function DeleteShort(req,res){
+    const { id } = req.params;
+    if(!id){ return res.sendStatus(404)};
     const { authorization } = req.headers;
     const token = authorization?.replace("Bearer ", "");
     if (!token) { return res.status(401).send('Unauthorized') };
-    const id = db.quer
+    try{
+    const response = await db.query('SELECT "userId" FROM sessions WHERE "Token"=$1',[token]);
+    if(response.rowCount == 0){ return res.status(401).send('Unauthorized') };
+    const userId = response.rows[0].userId;
+    const short = await db.query('SELECT "userId" FROM urls WHERE id=$1',[id])
+    if(userId != short.rows[0].userId){return res.status(401).send('Unauthorized')};
+    await db.query('DELETE FROM urls WHERE id=$1',[id]);
+    return res.sendStatus(204);
+    }
+    catch(err){
+        res.status(500).send(err);
+    }
 }
--- src/routes/url.routes.js
+++ src/routes/url.routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
-import { createShorten, getShorten, redirect } from "../controllers/url.controller.js";
+import { DeleteShort, createShorten, getShorten, redirect } from "../controllers/url.controller.js";
 
 const urlRoutes = Router();
 
 urlRoutes.post('/urls/shorten',createShorten);
 urlRoutes.get('/urls/:id',getShorten);
 urlRoutes.get('/urls/open/:shortUrl',redirect);
+urlRoutes.delete('/urls/:id',DeleteShort)
 
 
 export default urlRoutes;
